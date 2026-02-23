4282b0efd399ea89f417f1750a56373bfc4212d9
saaqibA21 <saaqibheroindia@gmail.com>
2026-02-23 15:08:30 +0530

    Add build script for Render to install both Python and Node dependencies

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Install Python dependencies
+echo "🐍 Installing Python dependencies..."
+pip install -r requirements.txt
+
+# Install Node.js dependencies
+echo "📦 Installing Node.js dependencies..."
+npm install
